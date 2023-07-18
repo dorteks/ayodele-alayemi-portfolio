3b46b40b32b943e1a9e860140ffe5f508dda990f
dorteks <ayodelealayemi@gmail.com>
2023-07-18 13:18:46 +0100

    [ft]: added marquee slider to home page

--- src/components/homepage/displayPictures.tsx
+++ src/components/homepage/displayPictures.tsx
@@ -1,47 +1,76 @@
 import React from "react";
 import Image from "next/image";
+import Marquee from "react-fast-marquee";
 
 const DisplayPictures = () => {
   return (
-    <div className="h-[350px] w-full py-4 my-10 tap-transparent">
-      <div className=" relative flex flex-row items-center justify-center  gap-8">
-        {/* <Image
-          src=""
-          width={300}
-          height={330}
-          alt="picture display"
-          className="h-[330px] w-[300px] rounded-2xl bg-inherit rotate-3"
-        /> */}
+    <Marquee className="flex flex-row gap-20">
+      <div className="flex flex-row gap-16 items-center h-[350px] my-10 tap-transparent ">
         <Image
-          src="/assets/frontend.jpg"
+          src="/assets/2020react-next.webp"
           width={300}
           height={330}
           alt="picture display"
-          className="h-[330px] w-[300px] rounded-2xl bg-inherit -rotate-3 "
+          className="h-[310px] w-[300px] rounded-2xl bg-inherit rotate-3"
         />
-        {/* <Image
-          src=""
+        <Image
+          src="/assets/frontend-dev.jpg"
           width={300}
           height={330}
           alt="picture display"
-          className="h-[330px] w-[300px] rounded-2xl bg-inherit rotate-2"
-        /> */}
-        {/* <Image
-          src=""
+          className="h-[310px] w-[300px] rounded-2xl bg-inherit -rotate-3 "
+        />
+        <Image
+          src="/assets/react-next.png"
           width={300}
           height={330}
           alt="picture display"
-          className="h-[330px] w-[300px] rounded-2xl bg-inherit rotate-3"
-        /> */}
-        {/* <Image
-          src=""
+          className="h-[310px] w-[300px] rounded-2xl bg-inherit rotate-2"
+        />
+        <Image
+          src="/assets/2020react-next.webp"
           width={300}
           height={330}
           alt="picture display"
-          className="h-[330px] w-[300px] rounded-2xl bg-inherit -rotate-3 "
-        /> */}
+          className="h-[310px] w-[300px] rounded-2xl bg-inherit rotate-3"
+        />
+        <Image
+          src="/assets/frontend-dev.jpg"
+          width={300}
+          height={330}
+          alt="picture display"
+          className="h-[310px] w-[300px] rounded-2xl bg-inherit -rotate-3 "
+        />
+        <Image
+          src="/assets/react-next.png"
+          width={300}
+          height={330}
+          alt="picture display"
+          className="h-[310px] w-[300px] rounded-2xl bg-inherit rotate-2"
+        />
+        <Image
+          src="/assets/2020react-next.webp"
+          width={300}
+          height={330}
+          alt="picture display"
+          className="h-[310px] w-[300px] rounded-2xl bg-inherit rotate-3"
+        />
+        <Image
+          src="/assets/frontend-dev.jpg"
+          width={300}
+          height={330}
+          alt="picture display"
+          className="h-[310px] w-[300px] rounded-2xl bg-inherit -rotate-3 "
+        />
+        <Image
+          src="/assets/react-next.png"
+          width={300}
+          height={330}
+          alt="picture display"
+          className="h-[310px] w-[300px] rounded-2xl bg-inherit rotate-2"
+        />
       </div>
-    </div>
+    </Marquee>
   );
 };
 
